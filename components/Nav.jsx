0e1a5d2c8b4e7c4760c82be1fe775fae418226c2
SanderKooi <119052501+SanderKooi@users.noreply.github.com>
2026-02-23 19:19:58 +0100

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { lekkageTypes, steden } from '../data'
+import { lekkageTypes, steden } from '../data' 
 
 const topSteden = steden.slice(0, 8)
 
