a6fa09e11e51827dd0075d9215674824d653b9c1
SanderKooi <119052501+SanderKooi@users.noreply.github.com>
2026-02-24 18:10:01 +0100

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://lekkagetest.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/404'],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+  },
+}
