49e7d7fe6ec1a36399fc8fa54893e5e88d61b7a7
SanderKooi <119052501+SanderKooi@users.noreply.github.com>
2026-02-25 17:59:17 +0100

    Update [stad].jsx

--- pages/lekkage/[stad].jsx
+++ pages/lekkage/[stad].jsx
@@ -2,7 +2,7 @@
 import Head from 'next/head'
 import Nav from '../../components/Nav'
 import { useState, useEffect } from 'react'
-import { steden, lekkageTypes, getSted } from '../../data'
+import { steden, lekkageTypes, getSted } from '../../data' 
 
 const PHONE = '0800-1234'
 const PHONE_DISPLAY = '0800-1234'
